4ac3b2685ebd60ae343f91fb5e79b0fb971a3359
Jasper Mayone <me@jaspermayone.com>
2024-04-27 15:53:42 -0400

    wip:

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,16 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Head, Html, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,50 +4,47 @@ import Link from "next/link";
 export default function Index() {
   return (
     <>
-      <header className="bg-black text-white p-2 sticky top-0 z-[1000] flex justify-between items-center">
-        <a className="text-2xl font-light" href="/">ham club</a>
+      {/* <header className="bg-black text-white p-2 sticky top-0 z-[1000] flex justify-between items-center">
+        <a className="text-2xl font-light" href="/">
+          ham club
+        </a>
         <nav>
           <ul className="flex gap-5 list-none m-0 p-0">
             <li className="flex gap-5">
-              <a className="hover:underline" href="/faq">faq</a>
-              {/* <a className="hover:underline" href="/faq">how it works</a> 
-              <a className="hover:underline" href="/faq">study links!</a> */}
+              <Link className="hover:underline" href="/faq">
+                faq
+              </Link>
+              faq
             </li>
           </ul>
         </nav>
-      </header>
-      <Image
-        className="sticky top-10 left-10"
-        src="/hackclub.svg"
-        alt="Logo"
-        width={125}
-        height={125}
-      />
-      <div className="pl-5 md:pl-0 pb-20">
-        <div className="w-screen text-wrap pt-5 md:pt-20 lg:pt-48">
-          <div className="flex justify-center">
+      </header> */}
+      <a className="banner" href="https://hackclub.com/">
+        <img
+          src="https://contribute.hackclub.com/images/flag-orpheus-top.png"
+          alt="Dinosaur arm holding flag that reads 'Hack Club'"
+        />
+      </a>
+      <div className="pb-2">
+        <hr className="dashed" />
+
+        <div className="w-screen text-wrap pt-2.5 md:pt-10 lg:pt-12">
+          <div className="flex">
             <Image
               src="/hamclub.svg"
               alt="Logo"
               width={500}
               height={500}
-              className="pr-20 pb-9 mx-auto"
+              className="pb-9 mx-auto"
             />
           </div>
-          <Link
-            href="https://hackclub.com/slack?event=HAM%20Night"
-            className="flex justify-center"
-          >
-            <button className="bg-black text-white rounded-2xl p-5 items-center mx-auto mt-10">
-              Join the Hack Club Slack!
-            </button>
-          </Link>
         </div>
         <div className="px-20 pt-20">
           <div className="prose flex mx-auto flex-col">
-            
-            <h1 className="w-full text-center">Welcome to Hack Club's Ham Club!</h1>
-            
+            <h1 className="w-full text-center">
+              Welcome to Hack Club&apos;s Ham Club!
+            </h1>
+
             <h1 className="w-full text-center">
               📻
               <span className="text-xl italic font-semibold">
@@ -57,17 +54,45 @@ export default function Index() {
               ✨
             </h1>
 
-            <p>Hey there! Are you ready to dive into the world of ham radio? This is where you'll learn all about radio communication, connect with a cool community, and earn your <span className="font-semibold">technician license</span>—the first step in your ham radio journey.</p>
+            <p>
+              Hey there! Are you ready to dive into the world of ham radio? This
+              is where you&apos;ll learn all about radio communication, connect
+              with a cool community, and earn your{" "}
+              <span className="font-semibold">technician license</span>—the
+              first step in your ham radio journey.
+            </p>
 
-            <p>We run a program that meets every night from <span className="font-semibold">8-9 PM Eastern Time</span> to study together. Don't worry if you can't make it to every session—you can always study alone and at your own pace. As long as you've passed the test by May 31st, you're on track to qualify for our grant, which covers the cost of your technician license.</p>
+            <p>
+              We run a program that meets every night from{" "}
+              <span className="font-semibold">8-9 PM Eastern Time</span> to
+              study together. Don&apos;t worry if you can&apos;t make it to
+              every session—you can always study alone and at your own pace. As
+              long as you&apos;ve passed the test by May 31st, you&apos;re on
+              track to qualify for our grant, which covers the cost of your
+              technician license.
+            </p>
 
-            <p>Plus, when you pass the exam, we'll send you a UV-K5 radio—perfect for diving into the world of ham radio and even doing some fun hacking!</p>
+            <p>
+              Plus, when you pass the exam, we&apos;ll send you a UV-K5
+              radio—perfect for diving into the world of ham radio and even
+              doing some fun hacking!
+            </p>
 
-            <p>Ham radio is more than just a hobby—it's a way to make real connections, help during emergencies, and get involved with local events like parades and marathons. Plus, it looks fantastic on college applications! So, come join our program, make some new friends, and let's get you on the air.</p>
+            <p>
+              Ham radio is more than just a hobby—it&apos;s a way to make real
+              connections, help during emergencies, and get involved with local
+              events like parades and marathons. Plus, it looks fantastic on
+              college applications! So, come join our program, make some new
+              friends, and let&apos;s get you on the air.
+            </p>
 
-            <i><p>Find us in #hamradio to study together!</p></i>
+            <i>
+              <p>Find us in #hamradio to study together!</p>
+            </i>
 
-            <p>Have Questions? Visit our <Link href="/faq">FAQ.</Link></p>
+            <p>
+              Have Questions? Visit our <Link href="/faq">FAQ.</Link>
+            </p>
             <div className="py-5" />
             <div className="md:flex md:flex-row md:space-x-5 pb-5 flex justify-center flex-col">
               <Card phase={1} />
