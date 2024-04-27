90734afe14586a3f9fc8c47ffaa7fef672d01d5d
Aram <me@aram.sh>
2024-04-27 09:26:13 -0700

    quick change

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,31 +4,36 @@ import Link from "next/link";
 export default function Index() {
   return (
     <>
-      <header className="header">
-        <Link className="logo" href="/">ham club</Link>
-        <nav>
-          <ul className="navList">
-            <li className="navItem">
-              <Link href="/faq">faq</Link>
-              <Link href="/faq">how it works</Link>
-              <Link href="/faq">study links!</Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
-      <div className="p-20">
-        <div className="w-screen text-wrap pt-0">
-          <Image
-            src="/hamclub.svg"
-            alt="Logo"
-            width={500}
-            height={500}
-            className="pr-20"
-          />
+      <Image
+        className="sticky top-0 left-10"
+        src="/hackclub.svg"
+        alt="Logo"
+        width={125}
+        height={125}
+      />
+      <div className="pl-5 md:pl-0 pb-20">
+        <div className="w-screen text-wrap pt-5 md:pt-20 lg:pt-48">
+          <div className="flex justify-center">
+            <Image
+              src="/hamclub.svg"
+              alt="Logo"
+              width={500}
+              height={500}
+              className="pr-20 pb-9 mx-auto"
+            />
+          </div>
+          <Link
+            href="https://forms.hackclub.com/t/oCU4yuMzGdus"
+            className="flex justify-center"
+          >
+            <button className="bg-black text-white rounded-2xl p-5 items-center mx-auto">
+              Get Your License!
+            </button>
+          </Link>
         </div>
         <div className="pt-20">
-          <div className="prose">
-            <h1>
+          <div className="prose flex mx-auto flex-col">
+            <h1 className="w-full text-center">
               📻
               <span className="text-xl italic font-semibold">
                 {" "}
@@ -40,63 +45,59 @@ export default function Index() {
               Have you ever looked at a radio and wondered, how does that thing
               work?
             </p>
-    
             <p>
-              I bet you{"'"}ve used a walkie before…
-              but have you have you ever radioed someone in space?
+              HAM radio is one of the oldest forms of communication with radios!
+              It{"'"}s where any licensed radio amateurs can communicate with
+              each other in a secret code only they know.{" "}
             </p>
             <p>
-              Ham radio is one of the oldest forms of remote communication.
-              With Ham radios, you can communicate with other ham radio users on
-              frequencies that regular radios just can{"'"}t use!
-              A regular walkie talkie can only communicate under a mile,
-              whereas ham radios can communicate 30 or more miles!
-            </p>
-            <p>
-              We want to make it easier than ever for you to join this super cool
-              and very exclusive club! From now until May 31st, if you get ship your
-              ham radio license, we will pay for your license exam, and <span className="font-semibold">ship you a ham radio!</span>
-            </p>
+              Heres the cool part anyone can get one! Including teens like you!
+              So now you{"'"}re thinking? How can I get to this secret club?
+              Well it does involve some studying and a test.
+            </p>{" "}
+            Hack Club wants to make easier than ever for you to join this secret
+            club.{" "}
             <span className="font-semibold">
-              How it works:{" "}
-            </span>
-            <p>
-              1{")"} We recommend you study for the next 14 days!
-              Collaborative Study Huddles will be happening from 8-9 PM Eastern Time
-              for the next 14 days in the #hamradio channel, and we would love to study with you!
-              (<i>you can totally start a study huddle at other times in the channel if you are inspired tho!</i>)
-            </p>
-            <p>
-              2{")"} Schedule & Take your Ham Exam!
-              (We will help you set one up if you need one, email ham@hackclub.com)
-            </p>
-            <p>
-              3{")"} Fill out our completion form
-              (link to be posted in #hamradio and on this website soon!)
-              once you have your license, and we{"'"}ll do the rest! Enjoy your ham!
-            </p>
-            <span className="italic">
-              <p>
-                Not required, but encouraged, post your progress in #scrapbook along the way!
-              </p>
-            </span>
+              So were running a experiment for all students in the US: Pass the
+              test and we will waive your exam fees, and we will buy you your
+              own radio!
+            </span>{" "}
+            All you need to do is post in #scrapbook in the{" "}
+            <Link href="https://hackclub.com/slack?event=HAM%20Night">
+              {" "}
+              Hack Club Slack
+            </Link>{" "}
+            on what you learned and join our nightly HAM nights!
+            <br/>
+            <p>Have Questions? Vist our <Link href="/faq">FAQ</Link></p>
             <div className="py-5" />
-            <div className="md:flex md:flex-row md:space-x-5 pb-5">
+            <div className="md:flex md:flex-row md:space-x-5 pb-5 flex justify-center flex-col">
               <Card phase={1} />
               <Card phase={2} />
             </div>
-            <div className="md:flex md:flex-row md:space-x-5">
+            <div className="md:flex md:flex-row md:space-x-5 flex justify-center flex-col">
               <Card phase={3} />
               <Card phase={4} />
             </div>
             <div className="py-5" />
-
+            <div className="flex justify-center">
+              <Link href="https://forms.hackclub.com/t/oCU4yuMzGdus">
+                <button className="bg-black text-white rounded-2xl p-5">
+                  Get Your License!
+                </button>
+              </Link>
+            </div>
           </div>
         </div>
       </div>
       <div className="w-screen h-48 bg-black">
         <div className="flex justify-center items-center h-full">
-          <p className="text-white">Made with love by Charlie, Aram, and Jasper for <Link className="underline" href="https://hackclub.com">Hack Club</Link></p>
+          <p className="text-white">
+            Made by{" "}
+            <Link className="text-2xl underline" href="https://hackclub.com">
+              Hack Club
+            </Link>
+          </p>
         </div>
       </div>
     </>
@@ -107,22 +108,22 @@ export function Card({ phase }: { phase: number }) {
   return (
     <>
       {phase === 1 && (
-        <div className="bg-cover rounded-2xl w-60 h-60 bg-[url('/images/1.png')]">
+        <div className="bg-cover rounded-2xl w-60 h-60 bg-[url('/images/1.png')] pt-5">
           <h2 className="text-white pl-3 pt-5 md:pt-0">Study!</h2>
         </div>
       )}
       {phase === 2 && (
-        <div className="bg-cover rounded-2xl w-60 h-60 bg-[url('/images/2.png')]">
+        <div className="bg-cover rounded-2xl w-60 h-60 bg-[url('/images/2.png')] pt-5">
           <h2 className="pl-[4rem]">Take the Exam!</h2>
         </div>
       )}
       {phase === 3 && (
-        <div className="bg-cover rounded-2xl w-60 h-60 bg-[url('/images/3.png')]">
+        <div className="bg-cover rounded-2xl w-60 h-60 bg-[url('/images/3.png')] pt-5">
           <h2 className="text-white pl-5 pt-[9rem]">Get a Radio!</h2>
         </div>
       )}
       {phase === 4 && (
-        <div className="bg-cover rounded-2xl w-60 h-60 bg-[url('/images/4.jpg')]">
+        <div className="bg-cover rounded-2xl w-60 h-60 bg-[url('/images/4.jpg')] pt-5">
           <h2 className="text-white pl-5">Explore!</h2>
         </div>
       )}
